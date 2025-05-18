f3b40498203b1e5e01c6d1b11bf29c9472bf1c68
jamie <hey@isuckat.dev>
2025-05-18 16:21:45 -0400

    init, added printing to screen/serial port + tests. i committed this way too late so i forget most of the stuff i actually added but whateverrr

---
+++ src/main.rs
@@ -0,0 +1,89 @@
+// cargo rustc -- -C link-args="/ENTRY:_start /SUBSYSTEM:console"
+// use the command above on windows
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga_buffer;
+mod serial;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+use core::panic::PanicInfo;
+
+#[cfg(not(test))]
+#[panic_handler] //calls on panic
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+static BOOTMSGA: &str = "starting ranaeOS...";
+static BOOTMSGB: &str = "this may take a while...";
+
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("{}", BOOTMSGA);
+    println!("{}", BOOTMSGB);
+
+    #[cfg(test)]
+    test_main();
+    //panic!("Panic test, should only show BOOTMSGA: {}", BOOTMSGA);
+    loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
