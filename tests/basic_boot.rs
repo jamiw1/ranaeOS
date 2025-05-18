3c23c3b90804d0785b3303891a908993d75803fe
jamie <hey@isuckat.dev>
2025-05-18 17:55:00 -0400

    tests, moved functions into lib.rs to allow other files to access data

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,30 +1,23 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(ranaeOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(ranaeOS::test_runner)]
 
 use core::panic::PanicInfo;
 use ranaeOS::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("starting ranaeOS...");
-    println!("this may take a while...");
-    
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
-#[cfg(test)]
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     ranaeOS::test_panic_handler(info)
